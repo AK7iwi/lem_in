2adcfded8c43d20c030f302489af244cac62b252
AK7iwi <atzlts@proton.me>
2026-01-12 12:40:20 +0100

    Visualizer: update SDL lib

--- srcs/visualizer/visualizer.c
+++ srcs/visualizer/visualizer.c
@@ -10,5 +10,6 @@ bool	visualizer(t_data *data)
 	set_render_params(&data->norm, data->map->nb_rooms);
 	if (render(renderer, data))
 		return (close_render(window, renderer), 1);
+
 	return (close_render(window, renderer), 0);
 }
